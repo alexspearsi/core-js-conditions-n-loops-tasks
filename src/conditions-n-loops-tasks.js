f959d7322e5be5a4eecd407f0d2488597c851b25
Alex <strelchenko.aleksandr@yandex.ru>
2024-12-19 13:57:30 +0200

    solve the task

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -66,8 +66,21 @@ function getMaxNumber(a, b, c) {
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  */
-function canQueenCaptureKing(/* queen, king */) {
-  throw new Error('Not implemented');
+function canQueenCaptureKing(queen, king) {
+  if (queen.x === king.x) {
+    return true;
+  }
+  if (queen.y === king.y) {
+    return true;
+  }
+  if (queen.y === queen.x && king.y === king.x) {
+    return true;
+  }
+  if (queen.y + queen.x === king.x + king.y) {
+    return true;
+  }
+
+  return false;
 }
 
 /**
